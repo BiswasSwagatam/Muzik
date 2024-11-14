f8ecc17a010db8ba8e8aa1785bdfa77c59ef5e12
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2024-11-14 18:26:01 +0530

    done

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -9,8 +9,11 @@ export const protectRoute = async (req, res, next) => {
 
 export const requireAdmin = async (req, res, next) => {
     try {
+        
         const currentUser = await clerkClient.users.getUser(req.auth.userId)
-        const isAdmin = process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress?.emailAddress
+        // const isAdmin = process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress?.emailAddress
+        const currentEmail = currentUser.primaryEmailAddress?.emailAddress
+        const isAdmin = process.env.ADMIN_EMAIL.includes(currentEmail)
 
         if(!isAdmin) {
             return res.status(403).json({message: "Unauthorized - you must be an admin"})
